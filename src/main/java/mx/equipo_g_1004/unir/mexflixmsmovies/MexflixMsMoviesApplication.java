161645819cb71b8dde9b26373bc6294cb705275a
AgnosticSix <mateo_godfather@hotmail.com>
2025-03-03 23:49:58 -0600

    initialize Mexflix MS Movies application with basic structure and configuration

---
+++ src/main/java/mx/equipo_g_1004/unir/mexflixmsmovies/MexflixMsMoviesApplication.java
@@ -0,0 +1,13 @@
+package mx.equipo_g_1004.unir.mexflixmsmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MexflixMsMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MexflixMsMoviesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/equipo_g_1004/unir/mexflixmsmovies/MexflixMsMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.equipo_g_1004.unir.mexflixmsmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MexflixMsMoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
